d769066c916862f4cab4143cde16fea31d6577f6
Juan <juan.sanmiguel@pragma.com.co>
2021-05-20 16:17:42 -0500

    Removed first attempt and unused classes

--- lib/logic/cubit/session/session_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'session_state.dart';
-
-class SessionCubit extends Cubit<SessionState> {
-  SessionCubit() : super(SessionTopTracks());
-}
--- lib/logic/cubit/session/session_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'session_cubit.dart';
-
-@immutable
-abstract class SessionState {}
-
-class SessionTopTracks extends SessionState {}
-
-class SessionSavedTracks extends SessionState {}
